61b36ad970e8332e5c0381d9ce2c043b1df15637
Miska Rautiainen <miksa.rautiainen@hotmail.com>
2021-02-17 00:27:12 +0200

    Major changes -Started using local questions instead of fetching them from API -Added questions.js file -Made changes to userview, finnish language etc.

--- questions.js
+++ questions.js
@@ -1,24 +1,57 @@
-//Fetch the data from API with fetch() method.
-window.onload = searchSports
-
-function searchSports() {
-  var url = "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
-
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((dataSports) => {
-      console.log(dataSports);
-      useApiData(dataSports)
-    });
-}
-
-function useApiData(dataSports){
-    document.querySelector("#question").innerHTML = `Question: ${dataSports.results[0].question}`
-    document.querySelector("#answer1").innerHTML = dataSports.results[0].correct_answer
-    document.querySelector("#answer2").innerHTML = dataSports.results[0].incorrect_answers[0]
-    document.querySelector("#answer3").innerHTML = dataSports.results[0].incorrect_answers[1]
-    document.querySelector("#answer4").innerHTML = dataSports.results[0].incorrect_answers[2]
-
-  }
+let questions = [
+    {
+    numb: 1,
+    question: "Millä nimellä kutsutaan Kiteen ABC liikenneaseman takana sijaitsevaa pajupusikkoa?",
+    answer: "Kallen baari",
+    options: [
+      "Heikin kulma",
+      "Kallen baari",
+      "Niilon nurkkaus",
+      "Tauskin taikametsä "
+    ]
+  },
+    {
+    numb: 2,
+    question: "Kuinka monta pesäpallofania mahtuu uudistuneen Kiteen rantakentän katetun katsomon alle?",
+    answer: "1500",
+    options: [
+      "800",
+      "1200",
+      "1500",
+      "2000"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Kuinka pitkä matka on Kiteen grilliltä Tolosenmäen Nesteelle?",
+    answer: "7,6km",
+    options: [
+      "5,5km",
+      "6,8km",
+      "7,6km",
+      "8,3km"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Missä Kiteen asuinosassa sijaitsee urheilukenttä, jota kutsutaan nimellä yläkenttä tai 'ylis'?",
+    answer: "Kytänniemessä",
+    options: [
+      "Sepänniemessä",
+      "Hutsissa",
+      "Ruppovaarassa",
+      "Kytänniemessä"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Mikä on Kiteenjärven keskisyvyys?",
+    answer: "2m",
+    options: [
+      "2m",
+      "3m",
+      "5m",
+      "7m"
+    ]
+  },
+];
