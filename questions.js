27e11d5d4807ae191e3a475e34a41df2997d6f2f
Miska Rautiainen <miksa.rautiainen@hotmail.com>
2021-02-18 02:51:52 +0200

    Style changes and a new questionnaire added -Modified app.css (added background picture and FA-icons) -Added 10 new questions and deleted the old ones

--- questions.js
+++ questions.js
@@ -1,57 +1,113 @@
 let questions = [
     {
     numb: 1,
-    question: "Millä nimellä kutsutaan Kiteen ABC liikenneaseman takana sijaitsevaa pajupusikkoa?",
-    answer: "Kallen baari",
+    question: "Kuinka paljon maksaa Hanna Partasen muikkukukko?",
+    answer: "26.00 €/Kg",
     options: [
-      "Heikin kulma",
-      "Kallen baari",
-      "Niilon nurkkaus",
-      "Tauskin taikametsä "
+      "18.00 €/Kg",
+      "23,50 €/Kg",
+      "26.00 €/Kg",
+      "31.00 €/Kg "
     ]
   },
     {
     numb: 2,
-    question: "Kuinka monta pesäpallofania mahtuu uudistuneen Kiteen rantakentän katetun katsomon alle?",
-    answer: "1500",
+    question: "Kuinka korkea on Puijon torni",
+    answer: "75m",
     options: [
-      "800",
-      "1200",
-      "1500",
-      "2000"
+      "45m",
+      "57m",
+      "75m",
+      "84m"
     ]
   },
     {
     numb: 3,
-    question: "Kuinka pitkä matka on Kiteen grilliltä Tolosenmäen Nesteelle?",
-    answer: "7,6km",
+    question: "Kuka seuraavista tunnetuista Suomalaisista on syntynyt Kuopiossa?",
+    answer: "Spede Pasanen",
     options: [
-      "5,5km",
-      "6,8km",
-      "7,6km",
-      "8,3km"
+      "Minna Canth",
+      "Risto Ryti",
+      "J. V. Snellman",
+      "Spede Pasanen"
     ]
   },
     {
     numb: 4,
-    question: "Missä Kiteen asuinosassa sijaitsee urheilukenttä, jota kutsutaan nimellä yläkenttä tai 'ylis'?",
-    answer: "Kytänniemessä",
+    question: "Montako askelmaa on Puijon hyppyrimäen portaissa?",
     options: [
-      "Sepänniemessä",
-      "Hutsissa",
-      "Ruppovaarassa",
-      "Kytänniemessä"
+      "221",
+      "312",
+      "376",
+      "410"
+    ]
+  },
+  
+    {
+    numb: 5,
+    question: "Missä sijaitsee kalakukkoaiheinen seinämaalaus?",
+    answer: "Keskustassa",
+    options: [
+      "Neulamäessä",
+      "Petosella",
+      "Männistössä",
+      "Keskustassa"
     ]
   },
     {
-    numb: 5,
-    question: "Mikä on Kiteenjärven keskisyvyys?",
-    answer: "2m",
+    numb: 6,
+    question: "Minä vuonna Matkuksen kauppakeskus avattiin?",
+    answer: "2012",
     options: [
-      "2m",
-      "3m",
-      "5m",
-      "7m"
+      "2010",
+      "2017",
+      "2012",
+      "2009"
     ]
   },
+    {
+    numb: 7,
+    question: "Mikä Minna Canth ei ollut koskaan ammatiltaan?",
+    answer: "Floristi",
+    options: [
+      "Floristi",
+      "Kirjailija",
+      "Kauppias",
+      "Journalisti"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Kuinka monta ihmistä mahtuu Kuopion keskuskentän katsomoihin yhteensä?",
+    answer: "4778",
+    options: [
+      "3198",
+      "7443",
+      "4778",
+      "1534"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Kuinka monta risteily-yritystä Kuopion satamassa toimii?",
+    answer: "Neljä",
+    options: [
+      "Kaksi",
+      "Neljä",
+      "Viisi",
+      "Ei yhtään"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Mikä on uuden Kuopion museon ja kaupunginkirjaston yhdistävän rakennuksen nimi?",
+    answer: "Kantti",
+    options: [
+      "Brahe",
+      "Kannus",
+      "Portti",
+      "Kantti"
+    ]
+  },
+  
 ];
